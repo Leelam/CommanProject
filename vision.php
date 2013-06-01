b1b792466ab9e8ad79fc32d5d6b91b0cd08aea43
maheshchimmiri <maheshchimmiri@gmail.com>
2013-06-02 01:17:17 +0530

    Vision
    change the page header

--- vision.php
+++ vision.php
@@ -16,7 +16,7 @@ include 'header.php';
     <div class="container">
       <div class="row-fluid">
       <div class="span12">
-              <h1>Company Profile</h1>
+              <h1>Vision</h1>
       </div>
       </div>
      </div>
