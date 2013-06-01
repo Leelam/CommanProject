dd251fbeb16453093465ab750ae82d6990db9370
Gopala Krishna <gm@leelam.com>
2013-06-02 01:57:43 +0530

    updatede the_Real_Content

--- vision.php
+++ vision.php
@@ -11,56 +11,53 @@ include 'header.php';
     <!-- Marketing messaging and featurettes
     ================================================== -->
     <!-- Wrap the rest of the page in another container to center all the content. -->
-    
-  <header class="page-header">
-    <div class="container">
+  <div id="the_real_content">  
+    <header class="page-header">
+      <div class="container">
+        <div class="row-fluid">
+          <div class="span12">
+            <h1>Vision</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+<div class="container">
       <div class="row-fluid">
-      <div class="span12">
-              <h1>Vision</h1>
-      </div>
-      </div>
-     </div>
-  </header>
-
-
-
-        <div class="container">
-          <div class="row-fluid">
         <div class="span9">
         	<ul>
 
-          <li>Our vision is to enable our clients to carry out their projects to create a more efficient and sustainable world, with better quality of life for society, generating returns
-</li>
-<li>
-	We shall be a professionally managed organization committed to total customer satisfaction
-</li>
-</ul>
-<ul>
-<h4><b>What We Believe</b></h4>
-<ul>
-<li><b>Client Orientation.</b> We anticipate our clients’ needs and target our actions to meet their goals.</li>
-<li><b>Financial Success.</b> Performing innovative, client-oriented, high-quality and sustainable work leads to new business, returning clients, and a reputation for excellence. By focusing on our core values and providing the best possible services to our clients, we ensure financial success.</li>
-<li><b>Passion.</b> We embrace our work with passion and enthusiasm. Passion drives the attention to detail and desire for success that ensures excellence. Passion is contagious and inspires others to work with the same energy and dedication.</li>
-</ul>
-            </div>
-            <div class="span3">
-<?php 
-include_once 'sidebar.php';
-?>
-            </div>
+            <li>Our vision is to enable our clients to carry out their projects to create a more efficient and sustainable world, with better quality of life for society, generating returns
+            </li>
+            <li>
+             We shall be a professionally managed organization committed to total customer satisfaction
+           </li>
+         </ul>
+         <ul>
+          <h4><b>What We Believe</b></h4>
+          <ul>
+            <li><b>Client Orientation.</b> We anticipate our clients’ needs and target our actions to meet their goals.</li>
+            <li><b>Financial Success.</b> Performing innovative, client-oriented, high-quality and sustainable work leads to new business, returning clients, and a reputation for excellence. By focusing on our core values and providing the best possible services to our clients, we ensure financial success.</li>
+            <li><b>Passion.</b> We embrace our work with passion and enthusiasm. Passion drives the attention to detail and desire for success that ensures excellence. Passion is contagious and inspires others to work with the same energy and dedication.</li>
+          </ul>
         </div>
-  </div>
 
+        <div class="span3">
+          <?php 
+          include_once 'sidebar.php';
+          ?>
+        </div>
+      </div>
+    </div>
+</div>
 
 
     <!-- Footer
     ================================================== -->
-  <?php
-  include 'footer.php';
-  
-  ?>
-    </body>
+    <?php
+    include 'footer.php';
+
+    ?>
+  </body>
   </html>
 
 
-   
