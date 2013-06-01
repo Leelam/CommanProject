1deb9499033589c72f91cf3493553ce9a899ae3b
maheshchimmiri <maheshchimmiri@gmail.com>
2013-06-02 02:18:51 +0530

    Tendering Services updated
    Tendering Services updated

---
+++ tenderingservices.php
@@ -0,0 +1,52 @@
+
+<?php
+include 'head.php';
+include 'header.php';
+?>
+
+    <!-- NAVBAR
+    ================================================== -->
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+    <div id="the_real_content">
+  <header class="page-header">
+    <div class="container">
+      <div class="row-fluid">
+      <div class="span12">
+              <h1>Tendering Services</h1>
+      </div>
+      </div>
+     </div>
+  </header>
+
+
+
+        <div class="container">
+          <div class="row-fluid">
+        <div class="span9">
+          <h4>Updating the content</h4>
+            </div>
+            <div class="span3">
+<?php  
+include_once 'sidebar.php';
+?>
+            </div>
+        </div>
+  </div>
+</div>
+
+
+    <!-- Footer
+    ================================================== -->
+  <?php
+  include 'footer.php';
+  
+  ?>
+    </body>
+  </html>
+
+
+   
