6e0ce7a294025d36559cd7323511e424d5fb2395
Gopala Krishna <gm@leelam.com>
2013-06-02 01:04:19 +0530

    updated footer

--- footer.php
+++ footer.php
@@ -13,44 +13,7 @@ Description: preperforably top
   <footer class="page-footer">
     <div class="container">
       <div class="row-fluid">
-      <div class="span4">
-            <h2>About US</h2>  
-            <div class="footer-content">
-hin our reset.less, we have removed some elements specifically for Bootstrap.With Bootstrap 2, the old reset block has been dropped in favor of Normalize.css, a project by Nicolas Gallagher and Jonathan Neal that also powers the HTML5 Boilerplate. While we use much of Normalize within our reset.less, we have removed some elements specifically for Bootstrap.With Bootstrap 2, the old reset block has been dropped in favor of Normalize.css, a project by Nicolas Gallagher and Jonathan Neal that also powers
-             </div>
-      </div>
-      <div class="span4">
-            <h2>Services</h2>
-            <div class="footer-content">  
-                <ul>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                  <li>Execution Team</li>
-                </ul>
-             </div>
-      </div>
-      <div class="span4">
-             <h2>Quick Contact</h2> 
-             <div class="footer-content">
-                <form>
-                  
-                  <input type="text" placeholder="Phone Number">
-                  
-                  <input type="text" placeholder="Phone Number">
-                  
-                  <textarea rows="4" placeholder="Message"></textarea> 
-
-                  <button class="btn">Submit</button>
-                </form>
-             </div>
-      </div>
+        <div class="span12">@ copyrights InfraDaily 2013. </div>
       </div>
      </div>
   </footer>
