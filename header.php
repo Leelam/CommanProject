28f754b1a59717a2c580813c5b39ed64b4568b5b
Gopala Krishna <gm@leelam.com>
2013-06-01 14:01:26 +0530

    updated v1.1

--- header.php
+++ header.php
@@ -3,7 +3,16 @@ Author : Gopala Krishna
 File : header.php
 
 ***********************************/
-?>
-<?php 
-include 'nav.php';
-?>
+?>  <body>
+   <!-- HEADER
+    ================================================== -->
+   <div class="container" style="height: 90px;">
+    <div class="row-fluid" style="padding-top: 10px;">
+      <div class="span4">
+        <h1><i class="icon-reorder"></i> InfraDaily</h1>
+      </div>
+      <div class="span8">
+        <?php include 'nav.php'; ?>
+      </div>
+    </div>
+   </div> 
