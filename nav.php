1740cfe85decf75125e1cde123acc1fbbae8da18
maheshchimmiri <maheshchimmiri@gmail.com>
2013-06-02 01:12:22 +0530

    file name changed as qualityassurance
    file name changed as qualityassurance

--- nav.php
+++ nav.php
@@ -13,7 +13,7 @@ Description: preperforably top
                     <li><a href="companyprofile.php">Company Profile</a></li>
                     <li><a href="directorsmessage.php">Director's Message</a></li>
                     <li><a href="vision.php">Vision</a></li>
-                    <li><a href="obectives.php">Qualit Polic & Obectives</a></li>
+                    <li><a href="qualityassurance.php">Qualit Polic & Obectives</a></li>
                 </ul>
             </li>
           <li class="dropdown">
